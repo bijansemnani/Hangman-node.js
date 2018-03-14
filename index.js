381bc82d4bdd7787ef71fcfc07fcbbb4bd9e1168
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-13 20:01:38 -0700

    made it so that if user wins they can play again

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var inquire = require("inquirer");
 var Word = require("./Word.js");
 
-
 var words = ["tiesto", "avicii", "alesso", "ram", "swedish house mafia"];
 var rand;
 var word;
 var count;
 var length;
+
 function setWord() {
   rand = words[Math.floor(Math.random()*words.length)];
   word = new Word(rand);
@@ -27,12 +27,30 @@ function recurPrompt(isFirst) {
   ]).then(function (answer) {
     word.guessWord(answer.letter);
     console.log(word.retWord());
+
     if (word.count === length) {
       console.log("You are correct!!!");
+      inquire.prompt([
+        {
+          type:"confirm",
+          name:"continue",
+          message:"Do you wish to play again?"
+        }
+
+      ]).then(function (answer) {
+        if (answer.continue) {
+          recurPrompt(true);
+        }
+        else {
+          console.log("Try again later then!");
+        }
+      });
     }
+
     else {
       recurPrompt(false);
     }
+    
   });
 }
 
